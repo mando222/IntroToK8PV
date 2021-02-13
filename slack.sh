80919214f08d3673063cfad621a0f1e13af20432
Eric Zietlow <evemsmando@gmail.com>
2021-02-13 10:05:32 -0600

    Create slack.sh

---
+++ slack.sh
@@ -0,0 +1,8 @@
+
+wget https://openebs.github.io/charts/openebs-operator.yaml
+kubectl get nodes
+kubectl get pods --all-namespaces
+wget https://openebs.github.io/charts/openebs-operator.yaml
+kubectl apply openebs-operator.yaml
+kubectl get pods --all-namespaces
+lsblk -f
